f248591f6e98fb6b0bb36f81c7b332d94f621eab
Timo Toups <ich@timotoups.de>
2016-03-11 14:21:55 +0100

    reverting changes

--- .config/zsh/themes/own-agnosta-light.zsh
+++ .config/zsh/themes/own-agnosta-light.zsh
@@ -102,8 +102,8 @@ prompt_git() {
     zstyle ':vcs_info:*' check-for-changes true
     zstyle ':vcs_info:*' stagedstr '✚'
     zstyle ':vcs_info:git:*' unstagedstr '●'
-    zstyle ':vcs_info:*' formats ' %u%c%m'
-    zstyle ':vcs_info:*' actionformats ' %u%c%m'
+    zstyle ':vcs_info:*' formats ' %u%c'
+    zstyle ':vcs_info:*' actionformats ' %u%c'
     vcs_info
     echo -n "${ref/refs\/heads\// }${vcs_info_msg_0_%% }${mode}"
   fi
