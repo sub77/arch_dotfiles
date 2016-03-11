d01177534af79f289d0982fcd1d93817e10e665a
Timo Toups <ich@timotoups.de>
2016-03-11 14:22:41 +0100

    moving location alias

--- .config/zsh/aliases/workstation.zsh
+++ .config/zsh/aliases/workstation.zsh
@@ -9,17 +9,6 @@ alias whatsmynetwork='ip addr show dev'
 alias -g v='vim'
 alias h='history'
 
-##DOTFILES
-alias my-github='cd ~/Github'
-alias dotfiles='cd ~/Github/dotfiles'
-alias config-dir='cd ~/.config'
-alias my-zsh='vim ~/.zshrc'
-alias my-antigen='vim ~/.config/zsh/antigen.zsh'
-alias my-aliases='vim ~/.config/zsh/aliases.zsh'
-alias my-shortcuts='vim ~/.config/zsh/shortcuts.zsh'
-alias my-vimrc='vim ~/.vimrc'
-alias my-xresources='vim ~/.Xresources'
-
 ##REMOTES
 alias -g licht='licht.pultschlampe.de'
 alias -g kinetik='kinetik.pultschlampe.de'
