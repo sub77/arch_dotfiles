36e983ad7b9cf378198dfc94e260ce18d1930ac3
Timo Toups <ich@timotoups.de>
2016-03-11 14:09:00 +0100

    added sublime to i3, modified bash promt to get a more visual diff to zsh prompt

--- .config/zsh/shortcuts.zsh
+++ .config/zsh/shortcuts.zsh
@@ -1,4 +1,15 @@
 ##GENERAL
-#github=~/github
 music=~/Music
 scripts=~/Documents/Scritps
+
+
+##DOTFILES
+alias my-github='cd ~/Github'
+alias my-dotfiles='cd ~/Github/dotfiles'
+alias my-config-dir='cd ~/.config'
+alias my-zsh='vim ~/.zshrc'
+alias my-antigen='vim ~/.config/zsh/antigen.zsh'
+alias my-aliases='vim ~/.config/zsh/aliases.zsh'
+alias my-shortcuts='vim ~/.config/zsh/shortcuts.zsh'
+alias my-vimrc='vim ~/.vimrc'
+alias my-xresources='vim ~/.Xresources'
