83fb897e7aecb885949615f26683d91c3b8fa7ea
broedli <timo@pultschlampe.de>
2016-03-12 18:39:17 +0100

    reverting

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,16 +2,6 @@
 set -e
 
 dir=`pwd`
-if [ ! -e "${dir}/setup.sh" ]; then
-  err "Script not called from within repository directory. Aborting."
-  exit 2
-fi
-
-distro=`lsb_release -si`
-if [ ! -f "dependencies-${distro}" ]; then
-  echo "Could not find file with dependencies for distro ${distro}. Aborting."
-  exit 2
-fi
 
 ask "Install packages?" N && bash ./dependencies-${distro}
 
