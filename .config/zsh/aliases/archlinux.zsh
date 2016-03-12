06108cdadd071b1a2fcd36c7b78cb54e8253e53e
broedli <timo@pultschlampe.de>
2016-03-12 17:15:59 +0100

    Added listing of installed packages (including AUR)

--- .config/zsh/aliases/archlinux.zsh
+++ .config/zsh/aliases/archlinux.zsh
@@ -102,7 +102,13 @@ alias pacmir='sudo pacman -Syy'                # Force refresh of all package li
 
 # https://bbs.archlinux.org/viewtopic.php?id=93683
 paclist() {
+  LC_ALL=C pacman -Qein $(pacman -Qu|cut -d" " -f 1)|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}'
+}
+paclist-all() {
   LC_ALL=C pacman -Qei $(pacman -Qu|cut -d" " -f 1)|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}'
+}
+aurlist() {
+  LC_ALL=C pacman -Qeim $(pacman -Qu|cut -d" " -f 1)|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}'
 }
 
 alias paclsorphans='sudo pacman -Qdt'
