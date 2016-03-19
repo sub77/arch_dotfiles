a7533c8dec569f0d06b73b9b395ac8f435a9d145
broedli <timo@pultschlampe.de>
2016-03-19 14:25:09 +0100

    added tmux

--- setup/setup.sh
+++ setup/setup.sh
@@ -23,8 +23,9 @@ msg "Linking dotfiles from $(textb $dir) to $(textb $HOME)."
 ask "Install symlink for $(textb .gitconfig)?" Y && ln -sfn ${dir}/.gitconfig $HOME/.gitconfig
 ask "Install symlink for $(textb .bashrc)?" Y && ln -sfn ${dir}/.bashrc $HOME/.bashrc && ln -sfn ${dir}/.bash.d $HOME/.bash.d
 ask "Install symlink for $(textb .zsh)?" Y && ln -sfn ${dir}/.zshrc $HOME/.zshrc && mkdir -p $HOME/.config && ln -sfn ${dir}/.config/zsh $HOME/.config/zsh
-ask "Install symlink for $(textb .colors)?" Y && ln -sfn ${dir}/.config/colors $HOME/.config/colors
 ask "Install symlink for $(textb .vim)?" Y && ln -sfn ${dir}/.vim $HOME/.vim && ln -sfn ${dir}/.vimrc $HOME/.vimrc
+ask "Install symlink for $(textb .colors)?" Y && ln -sfn ${dir}/.config/colors $HOME/.config/colors
+ask "Install symlink for $(textb .tmux)?" Y && ln -sfn ${dir}/.tmux.conf $HOME/.tmux.conf
 ask "Install symlink for $(textb .Xresources)?" Y && ln -sfn ${dir}/.Xresources $HOME/.Xresources
 ask "Install symlink for $(textb .termite)?" Y && ln -sfn ${dir}/.config/termite $HOME/.config/termite
 
